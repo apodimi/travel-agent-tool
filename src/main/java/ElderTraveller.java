71f93af60b74a9be9771dbd51907088bc76fa2ec
Apos_Dimit <aposdimit@gmail.com>
2021-04-06 16:57:15 +0300

    Add files via upload

---
+++ src/main/java/ElderTraveller.java
@@ -0,0 +1,14 @@
+public class ElderTraveller extends Traveller {
+
+    @Override
+    public double calculateSimilarity(City city) {
+
+        return 0;
+    }
+
+    //Constructor
+    public ElderTraveller(){
+
+    }
+}
+
---
+++ src/main/java/MiddleTraveller.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MiddleTraveller extends Traveller {
+
+    @Override
+    public double calculateSimilarity(City city) {
+
+        return 0;
+    }
+
+    //Constructor
+    public MiddleTraveller(){
+
+    }
+}
+
