457f1b1bfe1fbca9b6b7e6dc4185e75e252fe7c4
Apos_Dimit <aposdimit@gmail.com>
2021-04-09 15:27:07 +0300

    CITY IS READY

--- src/main/java/City.java
+++ src/main/java/City.java
@@ -1,16 +1,16 @@
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.*;
+
 
 public class City {
 
     private static String name;
-    private static int terms_vector;
+    private static int terms_vector[] = new int[10];
     private static double geodesic_vector[] = new double[2];
 
     //ArrayList for the City Object
-    List<City> cityObject = new ArrayList<>();
+    static ArrayList<City> cityObject = new ArrayList<>();
 
     //Constructor
     public City(){
@@ -28,11 +28,21 @@ public class City {
     }
 
     public static int getTerms_vector() {
-        return terms_vector;
+        return terms_vector.length;
     }
 
-    public static void setTerms_vector(int terms_vector) {
-        City.terms_vector = terms_vector;
+    public static void setTerms_vector(int theCafeNumber,int  theSeaNumber,int theMuseumsNumber,int theRestaurantNumber,int theStadiumNumber,int theParkNumber,int theClubNumber,int theFestivalNumber,int theHospitalNumber,int theTheaterNumber) {
+        terms_vector[0] = theCafeNumber;
+        terms_vector[1] = theSeaNumber;
+        terms_vector[2] = theMuseumsNumber;
+        terms_vector[3] = theRestaurantNumber;
+        terms_vector[4] = theStadiumNumber;
+        terms_vector[5] = theParkNumber;
+        terms_vector[6] = theClubNumber;
+        terms_vector[7] = theFestivalNumber;
+        terms_vector[8] = theHospitalNumber;
+        terms_vector[9] = theTheaterNumber;
+
     }
 
     public static double[] getGeodesic_vector() {
@@ -46,27 +56,45 @@ public class City {
     }
 
 
-    public int findTheTermsForTheCity() throws IOException {
+    public static int findTheTermsForTheCity() throws IOException {
+
+        City newCity = new City();
 
         //API KEY for the API
         String appid ="dd507317accd64c8447a7fadba863c9d";
-
         String city = "Rome";
         String country = "it";
 
         //Call the method and take the data from OpenWeatherMap and Wikipedia
-        OpenData.RetrieveData(city,country,appid);
+        String theWikipediaText = OpenData.RetrieveData(city,country,appid);
 
-        ///Set the name of the city witch user choose
+        //Set the name of the city witch user choose
         setName(city);
 
-        //System.out.println(geodesic_vector[1]);
+        int theCafeNumber = CountWords.countCriterionfCity(theWikipediaText,"cafe");
+        int theSeaNumber = CountWords.countCriterionfCity(theWikipediaText,"sea");
+        int theMuseumsNumber = CountWords.countCriterionfCity(theWikipediaText,"museums");
+        int theRestaurantNumber = CountWords.countCriterionfCity(theWikipediaText,"restaurant");
+        int theStadiumNumber = CountWords.countCriterionfCity(theWikipediaText,"stadium");
+        int theParkNumber = CountWords.countCriterionfCity(theWikipediaText,"park");
+        int theClubNumber = CountWords.countCriterionfCity(theWikipediaText,"club");
+        int theFestivalNumber = CountWords.countCriterionfCity(theWikipediaText,"festival");
+        int theHospitalNumber = CountWords.countCriterionfCity(theWikipediaText,"hospital");
+        int theTheaterNumber = CountWords.countCriterionfCity(theWikipediaText,"theater");
+
+        setTerms_vector(theCafeNumber,theSeaNumber,theMuseumsNumber,theRestaurantNumber,theStadiumNumber,theParkNumber,theClubNumber,theFestivalNumber,theHospitalNumber,theTheaterNumber);
+
+        cityObject.add(newCity);
+
+        //Tester
+        //System.out.println(cityObject.get(0).getName());
+
+
 
         return 0;
     }
 
 
-    /*Connect 2 projects : https://stackoverflow.com/questions/31342689/how-do-i-use-classes-from-another-project-in-intellij-idea*/
 
 
 }
